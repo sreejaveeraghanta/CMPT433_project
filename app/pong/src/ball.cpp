29f0112778609c8b2568f07491b4f5cfba0f496a
Gabriel Cheng <gabriel@debian.com>
2025-04-09 06:29:34 -0700

    re-added deleted led_strip

--- app/pong/src/ball.cpp
+++ app/pong/src/ball.cpp
@@ -1,5 +1,6 @@
 #include "ball.h"
 #include "sounds.h"
+#include "led_strip.h"
 
 #include <random>
 #include <cstdlib>  // For rand()
@@ -24,6 +25,8 @@ void Ball::move(float dt, int windowWidth, Player& player1, Player& player2)
         Position.x = Size.x;
         player2.Score += 1;
         sound_play_score();
+        opponent_scored_increment();
+        check_hit();
         reset();
     }
     // Right edge
@@ -33,6 +36,8 @@ void Ball::move(float dt, int windowWidth, Player& player1, Player& player2)
         Position.x = windowWidth - Size.x;
         player1.Score+=1;
         sound_play_score();
+        player_scored_increment();
+        check_hit();
         reset();
     }
     // Top edge
